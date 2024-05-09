830762c8a10fa923065c7f7e77851069cdbc113f
Código Café <contato@codigo.cafe>
2024-05-09 18:13:04 -0300

    Refactor: converter WS critterpedia-fish em JS para TS

---
+++ critterpedia-fish.js
@@ -0,0 +1,157 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var utils_1 = require("./acnh/utils");
+var dom_1 = require("./acnh/dom");
+var node_html_parser_1 = require("node-html-parser");
+var files_1 = require("./acnh/files");
+var benchmark_start = 0;
+var benchmark_end = 0;
+var catch_html = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var page_url, response_html, html, document_parsed;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                benchmark_start = performance.now();
+                page_url = 'https://nookipedia.com/wiki/Fish/New_Horizons';
+                return [4 /*yield*/, fetch(page_url, { method: 'GET' })];
+            case 1:
+                response_html = _a.sent();
+                return [4 /*yield*/, response_html.text()];
+            case 2:
+                html = _a.sent();
+                document_parsed = (0, node_html_parser_1.default)(html);
+                benchmark_end = performance.now();
+                console.log((0, utils_1.print_log_message)("Run 1: Capturando do HTML.", (benchmark_end - benchmark_start)));
+                return [2 /*return*/, document_parsed];
+        }
+    });
+}); };
+var get_months = function (list, condition_truthy) {
+    var response = [[]];
+    list.forEach(function (item, index) {
+        var actived_months_by_fish = [];
+        item.querySelectorAll('span').forEach(function (month) {
+            var actived_month = (month.getAttribute('style').indexOf(condition_truthy) > -1) ? true : false;
+            actived_months_by_fish.push(actived_month);
+        });
+        response[index] = actived_months_by_fish;
+    });
+    return response;
+};
+var init = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var document_parsed, content_wrapper, names, icons, prices, shadow_sizes, locations, times, wrapper_north_months, wrapper_south_months, north_months, south_months, fish, file_saved;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, catch_html()];
+            case 1:
+                document_parsed = _a.sent();
+                benchmark_start = performance.now();
+                content_wrapper = document_parsed.querySelector('#mw-content-text .tabletop table tbody');
+                names = (0, dom_1.domget)(content_wrapper).text('tr > td:nth-child(2)');
+                icons = (0, dom_1.domget)(content_wrapper).image('tr > td:nth-child(3) > a > img');
+                prices = (0, dom_1.domget)(content_wrapper).number('tr > td:nth-child(4)');
+                shadow_sizes = (0, dom_1.domget)(content_wrapper).text('tr > td:nth-child(5)');
+                locations = (0, dom_1.domget)(content_wrapper).text('tr > td:nth-child(6)');
+                times = (0, dom_1.domget)(content_wrapper).text('tr > td:nth-child(7)');
+                wrapper_north_months = (0, dom_1.domget)(content_wrapper).node('tr > td:nth-child(8) > span');
+                wrapper_south_months = (0, dom_1.domget)(content_wrapper).node('tr > td:nth-child(8) > p > span');
+                north_months = get_months(wrapper_north_months, '#50b3d4');
+                south_months = get_months(wrapper_south_months, '#50b3d4');
+                benchmark_end = performance.now();
+                console.log((0, utils_1.print_log_message)("Run 2: Identificação das informações.", (benchmark_end - benchmark_start)));
+                benchmark_start = performance.now();
+                fish = names.map(function (value, index) {
+                    var all_day = (times[index] === 'All day') ? true : false;
+                    var local_file_name = "".concat((0, utils_1.to_slug)(names[index]), ".").concat((0, utils_1.get_extension)(icons[index]));
+                    return {
+                        name: names[index],
+                        price: prices[index],
+                        icon: {
+                            local: "/files/fish/".concat(local_file_name),
+                            remote: icons[index]
+                        },
+                        shadow_size: shadow_sizes[index],
+                        location: locations[index],
+                        time: {
+                            all_day: all_day,
+                            hours: {
+                                initial: (!all_day) ? times[index].split(' – ')[0] : 0,
+                                finish: (!all_day) ? times[index].split(' – ')[1] : 0
+                            }
+                        },
+                        months: {
+                            north: north_months[index],
+                            south: south_months[index]
+                        },
+                        catch: false,
+                        donated: false
+                    };
+                });
+                benchmark_end = performance.now();
+                console.log((0, utils_1.print_log_message)('Run 3: Manipulação do DOM.', (benchmark_end - benchmark_start)));
+                benchmark_start = performance.now();
+                return [4 /*yield*/, (0, files_1.save_json)('./files', 'critterpedia-fish.json', fish)];
+            case 2:
+                file_saved = _a.sent();
+                benchmark_end = performance.now();
+                console.log((0, utils_1.print_log_message)('Run 4: Salvar arquivo JSON.', (benchmark_end - benchmark_start)));
+                benchmark_start = performance.now();
+                fish.map(function (fish) { return __awaiter(void 0, void 0, void 0, function () {
+                    var response_image, buffer_image, file_name, image_saved;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0: return [4 /*yield*/, fetch(fish.icon.remote, { method: 'GET' })];
+                            case 1:
+                                response_image = _a.sent();
+                                return [4 /*yield*/, response_image.arrayBuffer()];
+                            case 2:
+                                buffer_image = _a.sent();
+                                file_name = "".concat((0, utils_1.to_slug)(fish.name), ".").concat((0, utils_1.get_extension)(fish.icon.remote));
+                                image_saved = (0, files_1.save_image)('./files/fish', file_name, buffer_image);
+                                return [2 /*return*/];
+                        }
+                    });
+                }); });
+                benchmark_end = performance.now();
+                console.log((0, utils_1.print_log_message)('Run 4: Salvar arquivos de Imagem.', (benchmark_end - benchmark_start)));
+                return [2 /*return*/];
+        }
+    });
+}); };
+init();
--- critterpedia-fish.ts
+++ critterpedia-fish.ts
@@ -1,101 +1,104 @@
-// TODO aplicar TypeScript
-// TODO criar teste em Jest
-// TODO aplicar algum design partterns
-// TODO converter CommonJS para ECMAScript
+import {get_extension, print_log_message, to_slug} from "./acnh/utils";
+import { domget } from "./acnh/dom";
+import parse from "node-html-parser";
+import {iFish} from "./interfaces/fish";
+import {save_image, save_json} from "./acnh/files";
 
-const HTMLParse = require('node-html-parser');
-const acnh = require('./acnh/files');
-const acnh_util = require('./acnh/utils');
-const {save_image, _to_slug} = require("./acnh/files");
+let benchmark_start:number = 0;
+let benchmark_end:number = 0;
 
-console.log("Captura de informações - Critterpedia Fish.")
-let benchmark_start = 0;
-let benchmark_end = 0;
-let fishes_datas = [];
+const catch_html = async () => {
+    benchmark_start = performance.now();
+    const page_url:string = 'https://nookipedia.com/wiki/Fish/New_Horizons';
+    const response_html:Response = await fetch(page_url, {method: 'GET'});
+    const html:string = await response_html.text();
+    const document_parsed:HTMLElement | any = parse(html);
+    benchmark_end = performance.now();
+    console.log(print_log_message("Run 1: Capturando do HTML.", (benchmark_end - benchmark_start)));
+    return document_parsed;
+}
+
+const get_months = ( list:NodeListOf<Element>, condition_truthy:string ):[boolean[]] => {
+    const response:[boolean[]] = [[]];
+    list.forEach((item:Element, index:number) => {
+        const actived_months_by_fish:boolean[] = [];
+        item.querySelectorAll('span').forEach((month:HTMLElement):void => {
+            const actived_month:boolean = ( month.getAttribute('style').indexOf(condition_truthy) > -1 ) ? true : false ;
+            actived_months_by_fish.push(actived_month);
+        });
+        response[index] = actived_months_by_fish;
+    });
+    return response;
+}
 
 const init = async () => {
-    try{
-        benchmark_start = performance.now();
-        const pageUrl = 'https://nookipedia.com/wiki/Fish/New_Horizons';
-        const responseHtml = await fetch(pageUrl, {method: 'GET'});
-        const html = await responseHtml.text();
-        const document = HTMLParse.parse(html, {
-            blockTextElements: {
-                script: false,
-                noscript: false,
-                style: false,
-                pre: false
-            }
-        });
-        benchmark_end = performance.now();
-        acnh_util.print_log_message("Run 1: Capturando HTML", (benchmark_end - benchmark_start));
 
-        benchmark_start = performance.now();
-        const content_wrapper = document.querySelector('#mw-content-text .tabletop table tbody');
-        const names = content_wrapper.querySelectorAll('tr > td:nth-child(2)').map((column) => column.textContent.trim() );
-        const prices = content_wrapper.querySelectorAll('tr > td:nth-child(4)').map((column) => column.textContent.replace('Bells', '').trim() );
-        const shadow_sizes = content_wrapper.querySelectorAll('tr > td:nth-child(5)').map((column) => column.textContent.trim() );
-        const locations = content_wrapper.querySelectorAll('tr > td:nth-child(6)').map((column) => column.textContent.trim() );
-        const times = content_wrapper.querySelectorAll('tr > td:nth-child(7)').map((column) => column.textContent.trim() );
-        const icons = content_wrapper.querySelectorAll('tr > td:nth-child(3) > a > img').map((column) => column.getAttribute('src') );
-        const wrapper_north_months = content_wrapper.querySelectorAll('tr > td:nth-child(8) > span');
-        const wrapper_south_months = content_wrapper.querySelectorAll('tr > td:nth-child(8) > p > span');
+    const document_parsed:HTMLElement = await catch_html();
 
-        fishes_datas = names.map( (fish_name, index) => {
-            const is_all_day = (times[index] === 'All day') ? true : false;
-            const initial_hours = (is_all_day) ? 0 : times[index].split(' – ')[0];
-            const finish_hours = (is_all_day) ? 0 : times[index].split(' – ')[1];
-            const north_months = wrapper_north_months[index].querySelectorAll('span').map((month) => month.getAttribute('style').indexOf('color: #50b3d4') > -1 ? true : false);
-            const south_months = wrapper_south_months[index].querySelectorAll('span').map((month) => month.getAttribute('style').indexOf('color: #50b3d4') > -1 ? true : false);
-            return {
-                name: fish_name,
-                price: prices[index],
-                icon: {
-                    local: `./files/fish/${_to_slug(fish_name)}.png`,
-                    remote: icons[index]
-                },
-                shadow_size: shadow_sizes[index],
-                location: locations[index],
-                time: {
-                    all_day: is_all_day,
-                    hours: {
-                        initial: initial_hours,
-                        finish: finish_hours
-                    }
-                },
-                months: {
-                    north: north_months,
-                    south: south_months
-                },
-                catch: false,
-                donated: false
-            };
-        });
-        benchmark_end = performance.now();
-        acnh_util.print_log_message('Run 2: Informações selecionadas do HTML',(benchmark_end - benchmark_start));
 
-        benchmark_start = performance.now();
-        acnh.save_json('./files', 'critterpedia.fish.json', fishes_datas);
-        benchmark_end = performance.now();
-        acnh_util.print_log_message('Run 3: JSON salvo no arquivo critterpedia.fish.json', (benchmark_end - benchmark_start))
-
-    }catch (error){
-        console.log(`Error: [${error}]`);
-    }
-
-}
-init().then(() => {
-    console.log();
-    console.log('Salvar inmagens - Critterpedia Fish.');
     benchmark_start = performance.now();
-    fishes_datas.map( async (fish) => {
-        const image_response = await fetch(fish.icon.remote, {method: 'GET'});
-        const image_buffer = await image_response.arrayBuffer();
-        const image_path = save_image('./files/fish', fish.name, image_buffer);
+    const content_wrapper:Element = document_parsed.querySelector('#mw-content-text .tabletop table tbody');
+    const names:string[] = domget(content_wrapper).text('tr > td:nth-child(2)');
+    const icons:string[] = domget(content_wrapper).image('tr > td:nth-child(3) > a > img');
+    const prices:number[] = domget(content_wrapper).number('tr > td:nth-child(4)');
+    const shadow_sizes:string[] = domget(content_wrapper).text('tr > td:nth-child(5)');
+    const locations:string[] = domget(content_wrapper).text('tr > td:nth-child(6)');
+    const times:string[] = domget(content_wrapper).text('tr > td:nth-child(7)');
+    const wrapper_north_months = domget(content_wrapper).node('tr > td:nth-child(8) > span');
+    const wrapper_south_months = domget(content_wrapper).node('tr > td:nth-child(8) > p > span');
+    const north_months:[boolean[]] = get_months(wrapper_north_months, '#50b3d4');
+    const south_months:[boolean[]] = get_months(wrapper_south_months, '#50b3d4');
+    benchmark_end = performance.now()
+    console.log(print_log_message("Run 2: Identificação das informações.", (benchmark_end - benchmark_start)))
+
+
+    benchmark_start = performance.now();
+    const fish:iFish[] = names.map((value:string, index:number):iFish => {
+        const all_day = (times[index] === 'All day') ? true : false;
+        const local_file_name:string = `${to_slug(names[index])}.${get_extension(icons[index])}`;
+        return {
+            name: names[index],
+            price: prices[index],
+            icon: {
+                local: `/files/fish/${local_file_name}`,
+                remote: icons[index]
+            },
+            shadow_size: shadow_sizes[index],
+            location: locations[index],
+            time: {
+                all_day: all_day,
+                hours: {
+                    initial: (!all_day) ? times[index].split(' – ')[0] : 0,
+                    finish: (!all_day) ? times[index].split(' – ')[1] : 0
+                }
+            },
+            months: {
+                north: north_months[index],
+                south: south_months[index]
+            },
+            catch: false,
+            donated: false
+        };
     });
     benchmark_end = performance.now();
-    acnh_util.print_log_message('Run 4: Arquivos salvos', (benchmark_end - benchmark_start));
+    console.log(print_log_message('Run 3: Manipulação do DOM.',(benchmark_end - benchmark_start)))
 
-    console.log();
-    console.log('Task finished.');
-}).catch((error) => console.log('Has error.' + error))
+
+    benchmark_start = performance.now();
+    const file_saved:boolean = await save_json('./files', 'critterpedia-fish.json', fish);
+    benchmark_end = performance.now();
+    console.log(print_log_message('Run 4: Salvar arquivo JSON.', (benchmark_end - benchmark_start)))
+
+
+    benchmark_start = performance.now();
+    fish.map( async (fish:iFish):Promise<void> => {
+        const response_image = await fetch(fish.icon.remote, {method: 'GET'});
+        const buffer_image = await response_image.arrayBuffer();
+        const file_name = `${to_slug(fish.name)}.${get_extension(fish.icon.remote)}`;
+        const image_saved = save_image('./files/fish', file_name, buffer_image );
+    });
+    benchmark_end = performance.now();
+    console.log(print_log_message('Run 4: Salvar arquivos de Imagem.', (benchmark_end - benchmark_start)))
+
+}
+init()
